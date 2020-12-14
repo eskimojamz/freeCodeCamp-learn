ce46200488d3fbc23f842d01af93eeb2d5ae27a1
James Kim <juleskondo@gmail.com>
2020-12-14 18:41:04 +0900

    Update Slice and Splice.js

--- Javascript Algorithms and Data Structures Certification/Basic Algorithm Scripting/Slice and Splice.js
+++ Javascript Algorithms and Data Structures Certification/Basic Algorithm Scripting/Slice and Splice.js
@@ -1,3 +1,8 @@
+//Insert all elements of arr1 in arr2 starting at index n
+
+//create new arr x from all elements of arr2 with slice()
+//use splice() to place all elements of arr1 in array x starting at index n
+//return this new sliced and spliced array x
 function frankenSplice(arr1, arr2, n) {
   let x = arr2.slice()
   x.splice(n, 0, ...arr1)
