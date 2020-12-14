086e966eb3cbe29ac68e37fca0eaeeaa4a50fefd
James Kim <juleskondo@gmail.com>
2020-12-14 18:37:21 +0900

    Update Falsy Bouncer.js

--- Javascript Algorithms and Data Structures Certification/Basic Algorithm Scripting/Falsy Bouncer.js
+++ Javascript Algorithms and Data Structures Certification/Basic Algorithm Scripting/Falsy Bouncer.js
@@ -1,6 +1,6 @@
-#Remove all falsy values from an array.
+//Remove all falsy values from an array.
 
-#create new array and use filter() to return values x of arr that have boolean values of true
+//create new array and use filter() to return values x of arr that have boolean values of true
 function bouncer(arr) {
   let newArr = arr.filter(x => !!x === true)
   return newArr
