865580e0001759fc74c7b286dd962779af5a6d2a
James Kim <juleskondo@gmail.com>
2020-12-15 22:06:01 +0900

    Create Constructors Prototypes Inheritance.js

---
+++ Javascript Algorithms and Data Structures Certification/Basic Algorithm Scripting/Object Oriented Programming/Constructors Prototypes Inheritance.js
@@ -0,0 +1,18 @@
+function Bird() { 
+  this.numLegs = 2  
+ }
+
+Bird.prototype.fly = () => 'I am flying!';
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+Penguin.prototype.fly = () => 'Alas, this is a flightless bird.'
+
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
