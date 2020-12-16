5e24097198895dbe05470e93f816717a4922bdfd
James Kim <juleskondo@gmail.com>
2020-12-16 23:32:08 +0900

    Create Closure.js

---
+++ Javascript Algorithms and Data Structures Certification/Object Oriented Programming/Closure.js
@@ -0,0 +1,6 @@
+function Bird() {
+  let weight = 15;
+  this.getWeight = () => weight
+}
+
+//In JavaScript, a function always has access to the context in which it was created. This is called closure.
