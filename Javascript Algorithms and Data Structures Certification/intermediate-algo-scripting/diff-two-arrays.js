941a2c07039543b414f3c2a1f0449c3c6e4dd9aa
James Kim <juleskondo@gmail.com>
2020-12-19 13:07:29 +0900

    Create diff-two-arrays.js

---
+++ Javascript Algorithms and Data Structures Certification/intermediate-algo-scripting/diff-two-arrays.js
@@ -0,0 +1,8 @@
+function diffArray(arr1, arr2) {
+  var newArr = [...arr1, ...arr2]
+  return newArr.filter(item => 
+    !arr1.includes(item) || !arr2.includes(item)
+  )
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
